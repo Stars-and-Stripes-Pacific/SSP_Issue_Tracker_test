b8af9cca023ed739492b7d62f44b79a178c35ec9
Stars-Take <98077959+stars-take@users.noreply.github.com>
2024-02-07 15:19:03 +0900

    set information to variable

--- teamsBot.js
+++ teamsBot.js
@@ -20,17 +20,19 @@ class TeamsBot extends TeamsActivityHandler {
         method: 'GET',
       };
       
+      var categories = '';
       const req = https.request(options, (res) => {
           res.on('data', (chunk) => {
               console.log(`BODY: ${chunk}`);
-              context.sendActivity('Echo: start');
+              //context.sendActivity('Echo: start');
+              categories = chunk;
           });
           res.on('end', () => {
               console.log('No more data in response.');
               //await context.sendActivity(`Echo: ${txt}`);
-              context.sendActivity(`Echo: ${chunk}`);
+              //context.sendActivity(`Echo: ${chunk}`);
               // By calling next() you ensure that the next BotHandler is run.
-              next();
+              //next();
 
           });
       })
@@ -41,77 +43,10 @@ class TeamsBot extends TeamsActivityHandler {
       
       req.end();
 
-      /*
-      const req = https.request('https://40af-202-212-180-65.ngrok-free.app/api/messages', (res) => {
-        res.on('data', (chunk) => {
-          
-    
-        });
-    
-        ....
-    
-      })
-      req.end();
-      */
-
-      /*
-      var url = "https://stripes.sharepoint.com/sites/SSPDIDevNS-Noriko--TestIssueTracker";
-      // SharePoint access
-      spauth.getAuth(url, {
-          username: "sekiya.takeichiro@stripes.com",
-          password: "1live@srealman",
-          online: true
-      }).then(options => {
-          // Log
-          console.log("Connected to SPO");
-      
-          // Code Continues in 'Generate the Request'
-          // Get the web
-          var info = $REST.Web(url)
-              .Lists("Category")
-              .getInfo();
-      
-          for (var key in options.headers) {
-              // Set the header
-              info.headers[key] = options.headers[key];
-          }
-          var categorystr = '';
-          // Execute the request, based on the method
-          request[info.method == "GET" ? "get" : "post"]({
-              headers: info.headers,
-              //url: info.url,
-              url: info.url + "/items",
-              body: info.data
-          }).then(
-              // Success
-              response => {
-                  //console.log(response);
-      
-                  var obj = JSON.parse(response).d;
-                  //console.log(obj);
-                  if (obj.results && obj.results.length > 0) {
-                      // Parse the results
-                      for (var i = 0; i < obj.results.length; i++) {
-                          // Log
-                          //console.log(obj.results[i]);
-                          console.log(obj.results[i]['Title']);
-                          categorystr += obj.results[i]['Title']
-                      }
-                  } else {
-                  }
-              },
-              // Error
-              error => {
-              }
-          );
-      });
-      */
-      /*
       //await context.sendActivity(`Echo: ${txt}`);
-      await context.sendActivity(`Echo: ${categorystr}`);
+      await context.sendActivity(`Echo: ${categories}`);
       // By calling next() you ensure that the next BotHandler is run.
       await next();
-      */
     });
 
     // Listen to MembersAdded event, view https://docs.microsoft.com/en-us/microsoftteams/platform/resources/bot-v3/bots-notifications for more events
